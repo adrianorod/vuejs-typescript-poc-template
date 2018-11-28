7bd9752b3bd7987c0f426b05e84e11fd92d2fadf
adrianorod <adrianoazevedo.r@gmail.com>
2018-11-28 15:29:06 -0200

    hotfix

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,7 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
-      path: '/example',
+      path: '/',
       name: 'example',
       component: () => import('./views/example.view/index.vue'),
     },
